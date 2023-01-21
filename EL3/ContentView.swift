98aeb77770fd45e5418a6fc3afe5097ecf65c1a6
larry <larrycomb@gmail.com>
2023-01-21 13:40:33 -0800

    5 attempt at commiting

---
+++ EL3/ContentView.swift
@@ -0,0 +1,89 @@
+import OpenAISwift
+import SwiftUI
+import UIKit
+
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    var scrollView = UIScrollView()
+    let leadingConstraint:NSLayoutConstraint!
+    let trailingConstraint:NSLayoutConstraint!
+    let topConstraint:NSLayoutConstraint!
+    let bottomConstraint:NSLayoutConstraint!
+    
+    private var client: OpenAISwift?
+    
+    func setup(view: UIView) {
+        client = OpenAISwift(authToken: "")
+    
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        let topConstraint = scrollView.topAnchor.constraint(equalTo: view.topAnchor)
+        let bottomConstraint = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        view.addSubview(scrollView)
+        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
+        
+    }
+    
+    func send(text:String,
+              completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: {result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+                
+            }
+        })
+        
+    }
+}
+
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self){ string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack{
+                TextField("Type Here...", text: $text)
+                Button("Send"){
+                    send()
+                }
+            }
+            
+        }
+        
+        .onAppear{
+            viewModel.setup()
+        }
+        .padding()
+    }
+    func send(){
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        
+        models.append("Me: \(text)")
+        viewModel.send(text: text) {response in
+            DispatchQueue.main.async {
+                self.models.append("ChatGPT: "+response)
+                self.text = ""
+            }
+        }
+    }
+}
---
+++ EL3/EL3App.swift
@@ -0,0 +1,17 @@
+//
+//  EL3App.swift
+//  EL3
+//
+//  Created by LARRY COMBS on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
