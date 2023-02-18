9adda8d84f63dd4430c60d46981f4ae31828d0c7
larry <larrycomb@gmail.com>
2023-02-17 16:33:25 -0800

    add api

--- EL3/ContentView.swift
+++ EL3/ContentView.swift
@@ -1,92 +1,54 @@
-import OpenAISwift
 import SwiftUI
-import UIKit
-
-final class ViewModel: ObservableObject {
-    init() {}
-    
-    var scrollView = UIScrollView()
-    let leadingConstraint:NSLayoutConstraint!
-    let trailingConstraint:NSLayoutConstraint!
-    let topConstraint:NSLayoutConstraint!
-    let bottomConstraint:NSLayoutConstraint!
-    
-    private var client: OpenAISwift?
-    
-    func setup(view: UIView) {
-        client = OpenAISwift(authToken: "")
-    
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        let leadingConstraint = scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
-        let trailingConstraint = scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        let topConstraint = scrollView.topAnchor.constraint(equalTo: view.topAnchor)
-        let bottomConstraint = scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        view.addSubview(scrollView)
-        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
-        
-    }
-    
-    func send(text:String,
-              completion: @escaping (String) -> Void) {
-        client?.sendCompletion(with: text,
-                               maxTokens: 500,
-                               completionHandler: {result in
-            switch result {
-            case .success(let model):
-                let output = model.choices.first?.text ?? ""
-                completion(output)
-            case .failure:
-                break
-                
-            }
-        })
-        
-    }
-}
-
+import OpenAISwift
 
 struct ContentView: View {
-    @ObservedObject var viewModel = ViewModel()
-    @State var text = ""
-    @State var models = [String]()
+    @State private var inputMessage = ""
+    @State private var messages = [Message]()
     
     var body: some View {
         VStack(alignment: .leading) {
-            ForEach(models, id: \.self){ string in
-                Text(string)
+            List(messages) { message in
+                Text(message.text)
+                    .padding()
+                    .background(message.isUser ? Color.green : Color.blue)
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
             }
             
-            Spacer()
-            
-            HStack{
-                TextField("Type Here...", text: $text)
-                Button("Send"){
-                    send()
+            HStack {
+                TextField("Type here...", text: $inputMessage)
+                Button("Send") {
+                    self.sendMessage()
                 }
             }
-            
+            .padding()
         }
-        //The view is not in scope because "setup" in not being referenced
-        // Adding another note to be deleted this is to see if commit push works
-        // Third Try at git commit
-        // Moved filed to new Mkdir and tried to commit 
-        .onAppear{
-            viewModel.setup()
-        }
-        .padding()
     }
-    func send(){
-        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+    
+    private func sendMessage() {
+        guard !inputMessage.trimmingCharacters(in: .whitespaces).isEmpty else {
             return
         }
         
-        
-        models.append("Me: \(text)")
-        viewModel.send(text: text) {response in
-            DispatchQueue.main.async {
-                self.models.append("ChatGPT: "+response)
-                self.text = ""
+        messages.append(Message(text: inputMessage, isUser: true))
+        let client = OpenAISwift(authToken: "")
+        client.sendCompletion(with: inputMessage, maxTokens: 500) { result in
+            switch result {
+            case .success(let model):
+                let response = model.choices.first?.text ?? ""
+                self.messages.append(Message(text: response, isUser: false))
+            case .failure(let error):
+                self.messages.append(Message(text: error.localizedDescription, isUser: false))
             }
         }
+        
+        inputMessage = ""
     }
 }
+
+struct Message: Identifiable {
+    let id = UUID()
+    let text: String
+    let isUser: Bool
+}
+
