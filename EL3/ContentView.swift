19e8eb3296ac366bb94505a1bbea12799370cf1d
larry <larrycomb@gmail.com>
2023-02-26 17:50:10 -0800

    remove api key

--- EL3/ContentView.swift
+++ EL3/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
         }
         
         messages.append(Message(text: inputMessage, isUser: true))
-        let client = OpenAISwift(authToken: "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")
+        let client = OpenAISwift(authToken: "")
         client.sendCompletion(with: inputMessage, maxTokens: 500) { result in
             switch result {
             case .success(let model):
