a97c86a40c680e6bc74fcdfa1321e82837290d94
larry <larrycomb@gmail.com>
2023-04-26 19:03:47 -0700

    fix error with an uninstall and reinstall pods

--- EL3/ContentView.swift
+++ EL3/ContentView.swift
@@ -40,7 +40,7 @@ struct ContentView: View {
         }
         
         messages.append(Message(text: inputMessage, isUser: true))
-        let client = OpenAISwift(authToken: "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")
+        let client = OpenAISwift(authToken: "")
         client.sendCompletion(with: inputMessage, maxTokens: 500) { result in
             switch result {
             case .success(let model):
